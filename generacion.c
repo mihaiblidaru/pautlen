7b92662c9d219d709a8d46a4db87528d9cd74f63
andrescalvente97 <andrescalvente97@gmail.com>
2018-09-20 20:42:23 +0200

    Corregidos multiplicar, menor y no

--- generacion.c
+++ generacion.c
@@ -20,7 +20,7 @@ void escribir_cabecera_bss(FILE* fpasm){
 /*
    Declaración (con directiva db) de las variables que contienen el texto de los mensajes para la identificación de errores en tiempo de ejecución.
    En este punto, al menos, debes ser capaz de detectar la división por 0.
-*/ 
+*/
 void escribir_subseccion_data(FILE* fpasm){
 	fprintf(fpasm, "segment .data\n");
 	fprintf(fpasm, "\tmsg_error_div_0 db \"Division por 0\",0\n");
@@ -59,14 +59,14 @@ void escribir_fin(FILE* fpasm){
 /*
    Función que debe ser invocada cuando se sabe un operando de una operación aritmético-lógica y se necesita introducirlo en la pila.
 nombre es la cadena de caracteres del operando tal y como debería aparecer en el fuente NASM
-es_variable indica si este operando es una variable (como por ejemplo b1) con un 1 u otra cosa (como por ejemplo 34) 
-con un 0. Recuerda que en el primer caso internamente se representará como _b1 y, sin embargo, 
+es_variable indica si este operando es una variable (como por ejemplo b1) con un 1 u otra cosa (como por ejemplo 34)
+con un 0. Recuerda que en el primer caso internamente se representará como _b1 y, sin embargo,
 en el segundo se representará tal y como esté en el argumento (34).
 */
 void escribir_operando(FILE* fpasm, char* nombre, int es_variable){
 	if(es_variable){
 		fprintf(fpasm, "\tpush dword _%s\n", nombre);
-	}else{ 
+	}else{
 		fprintf(fpasm, "\tpush dword %s\n", nombre);
 	}
 }
@@ -88,13 +88,13 @@ Se extrae de la pila los operandos
 Se realiza la operación
 Se guarda el resultado en la pila
    Los dos últimos argumentos indican respectivamente si lo que hay en la pila es una referencia a un valor o un valor explícito.
-   Deben tenerse en cuenta las peculiaridades de cada operación. En este sentido sí hay que mencionar explícitamente que, en el caso de la división, 
-   se debe controlar si el divisor es “0” y en ese caso se debe saltar a la rutina de error controlado 
+   Deben tenerse en cuenta las peculiaridades de cada operación. En este sentido sí hay que mencionar explícitamente que, en el caso de la división,
+   se debe controlar si el divisor es “0” y en ese caso se debe saltar a la rutina de error controlado
    (restaurando el puntero de pila en ese caso y comprobando en el retorno que no se produce “Segmentation Fault”)
 */
 void restar(FILE* fpasm, int es_variable_1, int es_variable_2){
   fprintf(fpasm, "\tmov eax, [esp + 4]\n");
-  
+
   if(es_variable_1)
     fprintf(fpasm, "\tmov eax, [eax]\n");
 
@@ -110,31 +110,32 @@ void restar(FILE* fpasm, int es_variable_1, int es_variable_2){
 }
 
 
-/* 
+/*
  * Se puede hacer un poco mejor:
  * Imul tiene tambien el formato:
  *    IMUL mem32 => EAX = EAX * mem_dword
- * 
+ *
  * O sea que el segundo operando se podria multiplicar directamente
  * sin cargarlo previamente en un registro.
- * 
- * Esto no lo tengas mucho en cuenta(probablemente lo dejemos tal como esta), porque 
- * en realidad solo ahoraria una instruccion con el parametro 2 
+ *
+ * Esto no lo tengas mucho en cuenta(probablemente lo dejemos tal como esta), porque
+ * en realidad solo ahoraria una instruccion con el parametro 2
  * cuando no es un puntero.
  */
 void multiplicar(FILE* fpasm, int es_variable_1, int es_variable_2){  /*DUDA*/
-  fprintf(fpasm, "\tmov eax, [esp]\n");
+
+  fprintf(fpasm, "\tmov eax, [esp]\n"); //Primer operando
 
   if (es_variable_1)
-    fprintf(fpasm, "\tmov eax, [eax]\n");
+    fprintf(fpasm, "\tmov eax, [eax]\n"); //Cogemos el contenido si es una viable
 
-  fprintf(fpasm, "\tmov ecx, [esp + 4]\n");
+  fprintf(fpasm, "\tmov ecx, [esp + 4]\n"); //Segundo operando
 
   if (es_variable_2)
-    fprintf(fpasm, "\tmov ecx, [ecx]\n");
+    fprintf(fpasm, "\tmov ecx, [ecx]\n"); //Cogemos el contenido si es una viable
 
-  fprintf(fpasm, "\timul ecx\n");
-  fprintf(fpasm, "\tpush eax\n");
+  fprintf(fpasm, "\timul ecx\n"); //Multiplicamos los dos operandos
+  fprintf(fpasm, "\tpush eax\n"); //Ponemos el resultado en la pila
 }
 
 //ESta bien
@@ -172,10 +173,10 @@ void y(FILE* fpasm, int es_variable_1, int es_variable_2){
 */
 void cambiar_signo(FILE* fpasm, int es_variable){
   fprintf(fpasm, "\tmov eax, [esp]\n");
-  
+
   if(es_variable)
     fprintf(fpasm, "\tmov eax, [eax]\n");
-  
+
   fprintf(fpasm, "\tneg eax\n");
   fprintf(fpasm, "\tpush eax\n");
 
@@ -183,38 +184,42 @@ void cambiar_signo(FILE* fpasm, int es_variable){
 
 /*
  * Esta está mal. Basicamente tiene varias cosas mal:
- * 
+ *
  * 1. Usas registros de solo 16 bits(aka ax).
  * 2. La instruccion NOT hace el complemento a 1, no la negacion descrita en el enunciado:
  *       Si en la pila lo que se niega es un 0 = 0x00000000 la idea
  *       es que pase a valer 1 = 0x00000001 pero usando NOT el resultado
  *       es 0xFFFFFFFF.
- * 3. el argumentos cuantos_no no se para que sirve, pero estoy bastante 
- * seguro de que no significa cuantas veces negar una cosa. Esto mejor se lo preguntamos 
+ * 3. el argumentos cuantos_no no se para que sirve, pero estoy bastante
+ * seguro de que no significa cuantas veces negar una cosa. Esto mejor se lo preguntamos
  * en clase o en un correo.
  */
 void no(FILE* fpasm, int es_variable, int cuantos_no){
   int i;
 
-  fprintf(fpasm, "\tmov ax, [esp]\n");
+  fprintf(fpasm, "\tmov ax, 00H\n");  //Limpiamos el registro AX de datos. OJO puede que tenga datos que estemos usando
+  fprintf(fpasm, "\tpop ax\n");  //Popeamos el elemento para después poner el nuevo
 
   if (es_variable)
-    fprintf(fpasm, "\tmov ax, [ax]\n");
+    fprintf(fpasm, "\tmov ax, [ax]\n"); //Cogemos el contenido si es una viable
 
-  for (i = 0; i < cuantos_no; i++)
-    fprintf(fpasm, "\tnot ax\n");
+  fprintf(fpasm, "\tcmp ax, 1\n");  //Vemos si es un 1
+  fprintf(fpasm, "\tje neg_%d\n", cuantos_no); //Si lo es saltamos
+  fprintf(fpasm, "\tpush dword 1\n", cuantos_no); //Si no, metes un 1 en la pila
+  fprintf(fpasm, "\tjmp contNo_%d\n", cuantos_no);  //Saltamos para continuar con el codigo
+  fprintf(fpasm, "neg_%d: push dword 0\n", cuantos_no); //Como era un 1, metemos un 0 en la pila
 
-  fprintf(fpasm, "\tpush ax\n");
+  fprintf(fpasm, "contNo_%d: \n", cuantos_no);  //Ponemos la etiqueta para seguir con el codigo
 }
 
-/* 
-   Todas estas funciones reciben como argumento si los elementos a comparar son o no variables. 
+/*
+   Todas estas funciones reciben como argumento si los elementos a comparar son o no variables.
    El resultado de las operaciones, que siempre será un booleano
     (“1” si se cumple la comparación y “0” si no se cumple), se deja en la pila como en el resto de operaciones.
      Se deben usar etiquetas para poder gestionar los saltos necesarios para implementar las comparaciones.
 */
 void igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
-  
+
   fprintf(fpasm, "\tmov eax, [esp]\n");
   if(es_variable1){
     fprintf(fpasm, "\tmov eax, [eax]\n");
@@ -223,7 +228,7 @@ void igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
   fprintf(fpasm, "\tmov ecx, [esp + 4]\n");
   if(es_variable2){
     fprintf(fpasm, "\tmov ecx, [ecx]\n");
-  }    
+  }
 
   fprintf(fpasm, "\tcmp eax, ecx\n");
   fprintf(fpasm, "\tje true_%d\n", etiqueta);
@@ -271,22 +276,28 @@ void mayor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
 
 
 void menor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
-  fprintf(fpasm, "\tmov eax, [esp + 4]\n"); /*Operador de la izquierda*/
+  fprintf(fpasm, "\tmov eax, [esp + 4]\n"); //Operador de la izquierda
 
   if (es_variable1)
-    fprintf(fpasm, "\tmov eax, [eax]\n");
+    fprintf(fpasm, "\tmov eax, [eax]\n"); //Cogemos el contenido si es una viable
 
-  fprintf(fpasm, "\tmov edx, [esp]\n"); /*Operador de la derecha*/
+  fprintf(fpasm, "\tmov edx, [esp]\n"); //Operador de la derecha
 
   if (es_variable2)
-    fprintf(fpasm, "\tmov edx, [ecx]\n");
+    fprintf(fpasm, "\tmov edx, [ecx]\n"); //Cogemos el contenido si es una viable
 
-  fprintf(fpasm, "\tjl %d\n", etiqueta); /*Repasar*/
+  fprintf(fpasm, "\tcmp eax, edx\n"); //Comparamos los dos registros
+  fprintf(fpasm, "\tjl true_%d\n", etiqueta); //Saltamos si eax es menor que edx
+  fprintf(fpasm, "\tpush dword 0\n"); //Metemos un 0 ya que no se cumple
+  fprintf(fpasm, "\tjmp contMenor_%d\n", etiqueta); //Saltamos para continuar con el programa
+  fprintf(fpasm, "true_%d: push dword 1\n", etiqueta);  //Etiqueta donde pondremos el 1 en la pila ya que se cumple
+
+  fprintf(fpasm, "contMenor_%d: \n", etiqueta); //Etiqueta para continuar con el codigo
 }
 
 
 void mayor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
-  
+
   fprintf(fpasm, "\tmov eax, [esp + 4]\n"); /*Operador de la izquierda*/
   if (es_variable1){
     fprintf(fpasm, "\tmov eax, [eax]\n");
@@ -294,7 +305,7 @@ void mayor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
 
   fprintf(fpasm, "\tmov ecx, [esp]\n"); /*Operador de la derecha*/
   if (es_variable2){
-    fprintf(fpasm, "\tmov ecx, [ecx]\n");  
+    fprintf(fpasm, "\tmov ecx, [ecx]\n");
   }
 
   fprintf(fpasm, "\tcmp eax, ecx\n");
